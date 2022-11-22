4fb2b4f1202515774f9e4088074943d7fc773e7e
ThaBeanBoy <gtchipoyera@gmail.com>
2022-11-22 15:40:16 +0200

    Made some minor updates

--- Program.cs
+++ Program.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ConsoleApp1{
+    class Program{
+        static void Main(string[] args){
             Console.WriteLine("Hello World");
 
             Console.ReadLine();
