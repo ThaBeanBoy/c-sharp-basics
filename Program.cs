f8b687e836ab17db2517e55c934bcc955599fe77
ThaBeanBoy <gtchipoyera@gmail.com>
2022-11-22 15:49:27 +0200

    Made some methods to experiment

--- Program.cs
+++ Program.cs
@@ -7,9 +7,24 @@ using System.Threading.Tasks;
 namespace ConsoleApp1{
     class Program{
         static void Main(string[] args){
-            Console.WriteLine("Hello World");
+            //Testing the default args
+            Console.WriteLine(GreetUser());
 
+            
+            Console.WriteLine(GreetUser("Lebron", "James"));
+
+            //Testing the named args
+            Console.WriteLine(GreetUser(
+                Name: "Steph",
+                Surname: "Curry"
+            ));
+
+            Console.WriteLine("\nPress Enter To Exit");
             Console.ReadLine();
+        }
+
+        static string GreetUser(string Name = "John", string Surname = "Doe"){
+            return String.Format("Hello there {0} {1}", Name, Surname);
         }
     }
 }
